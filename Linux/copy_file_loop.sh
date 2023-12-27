d759b64fab3cee062452e5278b4c6ec640be564a
Zhan Ziyuan <154674404+ZhanZiyuan@users.noreply.github.com>
2023-12-27 11:21:26 +0800

    Create copy_file_loop.sh
    First upload.

---
+++ Linux/copy_file_loop.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# file path of the file to be copied
+old_path="/home/zhanziyuan/python_assignments/yaml/conformation/conformation_eq_run/"
+
+# file path of the file copied
+new_path="/home/zhanziyuan/python_assignments/yaml/conformation/conformation_eq_tree/conformation_eq_tree_run/"
+
+# original prefix of the file name 
+old_prefix="conformation_eq_pea_"
+
+# modified prefix of the file name 
+new_prefix="conformation_eq_pea_"
+
+# original extension of the file name 
+old_extension=".py"
+
+# modified extension of the file name 
+new_extension=".py"
+
+# loop
+for ((i=1; i<=11; i++)); do
+
+    # use `printf` to create a new file name; 
+    # `%02d` means "format the integer with 2 digits, left padding it with zeroes"
+    old_filename=$(printf "${old_path}${old_prefix}%02d${old_extension}" $i)
+    new_filename=$(printf "${new_path}${new_prefix}%02d${new_extension}" $i)
+
+    cp "$old_filename" "$new_filename"
+
+    n=$(printf "%02d" $i)
+
+    # print the loop count
+    echo "+-----------------------+"
+    echo "|                       |"
+    echo "  File No. ${n} copied.  "
+    echo "|                       |"
+    echo "+-----------------------+"
+
+done
+
+echo "+-----------------------+"
+echo "|                       |"
+echo "| All files are copied. |"
+echo "|                       |"
+echo "+-----------------------+"
