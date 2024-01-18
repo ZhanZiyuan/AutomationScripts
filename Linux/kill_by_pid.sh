8a98969e126d0df6d97700c2fbee15346e80b403
Zhan Ziyuan <154674404+ZhanZiyuan@users.noreply.github.com>
2024-01-18 10:29:55 +0800

    Update and rename kill_processes.sh to kill_by_pid.sh
    Renamed `kill_processes.sh` to `kill_by_pid.sh`.

--- Linux/kill_by_pid.sh
+++ Linux/kill_by_pid.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 
+# Please refer to:
+# https://xstarcd.github.io/wiki/shell/shell_special_variables.html
+# https://developer.aliyun.com/article/530776
+
 # Check if at least one parameter is passed
 if [ "$#" -eq 0 ]; then
     echo "Please pass at least one PID as a valid parameter."
     exit 1
 fi
 
-# Array to store terminated PIDs
+# Array to store PIDs of killed processes
 terminated_pids=()
 
 # Process each passed PID in a loop
 for pid in "$@"; do
     # Check if the passed PID exists
     if ps -p "$pid" > /dev/null; then
-        # Stop the process
+        # Kill the process
         printf "Killing process of PID: $pid\n"
         kill -9 "$pid"
-        # Add the terminated PID to the array
+        # Add the PID of the killed process to the array
         terminated_pids+=("$pid")
     else
         printf "Process of PID: $pid does not exist.\n"
     fi
 done
 
-# Print all terminated PIDs
+# Print PIDs of killed processes
 if [ ${#terminated_pids[@]} -eq 0 ]; then
     echo "No processes were killed."
 else
     printf "Process(es) of PID(s): ${terminated_pids[*]} killed.\n"
 fi
+
 
