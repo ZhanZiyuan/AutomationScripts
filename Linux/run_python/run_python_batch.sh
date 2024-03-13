f4a2dc92a1768dba01ae3bb9fb3fec663d2a5d95
Zhan Ziyuan <154674404+ZhanZiyuan@users.noreply.github.com>
2024-03-13 10:58:06 +0800

    Update run_python_batch.sh
    Sbatch headers and annotations modified.

--- Linux/run_python/run_python_batch.sh
+++ Linux/run_python/run_python_batch.sh
@@ -3,10 +3,9 @@
 #SBATCH --partition=S01
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=1
 #SBATCH --job-name=python_job
 #SBATCH --nodelist=node8
-#SBATCH --output=python_job.%j.out
 
 
 # file path of the Python script to run
@@ -18,16 +17,16 @@ log_file_path=$2
 # run the Python script; redirect the output to a .log file
 python "$python_file_path" > "$log_file_path" 2>&1
 
-# print a prompt
-printf "Python task submitted. PID: $!\n"
 
-
-# An example of running this batch script:
+# Usage
 #
 # Please refer to:
 # - [sbatch](https://hpc.pku.edu.cn/_book/guide/slurm/sbatch.html)
 # - [special variables of shell](https://xstarcd.github.io/wiki/shell/shell_special_variables.html)
+# - [Slurm job scheduling system](https://docs.hpc.sjtu.edu.cn/job/slurm.html)
 #
-# This batch script always runs on node8:
+# An example of running this batch script:
 #
 # sbatch run_python_batch.sh test_01.py output_01.log
+# 
+# ATTENTION: computational tasks submitted by this batch script always runs on node8.
